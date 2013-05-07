223bc58c9dbe49e1543a6129d9d0ebb6b4684c59
cachemoney <cachebroker@gmail.com>
2013-05-06 20:36:40 -0500

    video model spec

---
+++ spec/models/video_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Video do
+	it "saves itself" do
+		video = Video.new(title: "test movie title", description: "test desc", 
+			small_cover_url: "small.jpg", large_cover_url: "large.jpg",
+			category_id: 5)
+		video.save
+		Video.first.title.should eq ("test movie title")
+	end
+end
