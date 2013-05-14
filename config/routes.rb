75cf6a8c9a824a93081b52658ff2bf9ddcf1a5d1
Robin <cachebroker@gmail.com>
2013-05-13 19:55:58 -0500

    added sign_in, register, and users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Myflix::Application.routes.draw do
 
-	resources :videos
-	match "/home"	=> "videos#index", as: "home"
+  root to: 'pages#front'
+	resources :videos, only: [:show] do
+		collection do
+			post :search, to: "videos#search"
+		end
+	end
   get 'ui(/:action)', controller: 'ui'
+  get 'register', to: 'users#new'
+  get 'sign_in', to: 'sessions#new'
+  resources :users, only: [:create]
 end
