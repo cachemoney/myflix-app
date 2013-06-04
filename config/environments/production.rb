e4d06a6dca05cf4074bdf068dfc722dac0195d25
Robin Paul <cachebroker@gmail.com>
2013-06-04 16:37:42 -0500

    modified url option

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Myflix::Application.configure do
     password: ENV["GMAIL_PASSWORD"]
   }
 
-  config.action_mailer.default_url_options = { :host => 'cache-myflix.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'vast-beyond-4945.herokuapp.com' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
