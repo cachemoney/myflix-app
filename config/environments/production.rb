3da7d6274d22f65c38a0c331fc106e20cd77bcab
Robin Paul <cachebroker@gmail.com>
2013-06-04 18:39:43 -0500

    changed mandrill port

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Myflix::Application.configure do
 
   config.action_mailer.smtp_settings = {
     :address   => "smtp.mandrillapp.com",
-    :port      => 25, # ports 587 and 2525 are also supported with STARTTLS
+    :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
     :enable_starttls_auto => true, # detects and uses STARTTLS
     :user_name => "MANDRILL_USERNAME",
     :password  => "MANDRILL_PASSWORD", # SMTP password is any valid API key
