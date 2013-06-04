c71643d269cfd219986e630c12c9c14f94cba44d
Robin Paul <cachebroker@gmail.com>
2013-06-04 16:28:17 -0500

    added domain to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Myflix::Application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "example.com",
+    domain: "vast-beyond-4945.herokuapp.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
