a5f436f4386847a44aff3465803781c3f9e33692
Robin Paul <cachebroker@gmail.com>
2013-06-04 19:18:12 -0500

    stupid mistake

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,8 +69,8 @@ Myflix::Application.configure do
     :address   => "smtp.mandrillapp.com",
     :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
     :enable_starttls_auto => true, # detects and uses STARTTLS
-    :user_name => "MANDRILL_USERNAME",
-    :password  => "MANDRILL_PASSWORD", # SMTP password is any valid API key
+    :user_name => ENV["MANDRILL_USERNAME"],
+    :password  => ENV["MANDRILL_API_KEY"], # SMTP password is any valid API key
     :authentication => 'login' # Mandrill supports 'plain' or 'login' 
   }
 
