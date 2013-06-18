d334e3e6a11d6c6537fa8fc9840f37e9c561efc0
Robin Paul <cachebroker@gmail.com>
2013-06-17 21:20:33 -0500

    Errors with video new

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -1,6 +1,10 @@
 class Admin::VideosController < AdminsController
 
 	def new
-		@video = Video.new
+		@video = Admin::Video.new
+	end
+
+	def create
+		@video = Admin::Video.new(params[:video])
 	end
 end
